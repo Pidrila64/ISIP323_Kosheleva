a750ceb3cf6e7cd93ba89b22ffbcb71b6c82f009
234959 <234959@KB37-303-C2>
2025-09-11 16:35:08 +0300

    первый коммит функция ввода операций

--- ISIP323_Kosheleva/Program.cs
+++ ISIP323_Kosheleva/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Console.WriteLine("Введите количество операций (от 2 до 40):");
+int count;
+while (!(int.TryParse(Console.ReadLine(), out count) && count >= 2 && count <= 40))
+{
+    Console.WriteLine("Ошибка! Введите число от 2 до 40:");
+}
+
