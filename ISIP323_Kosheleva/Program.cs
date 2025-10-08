e72063a087e2835b53df447e50ba5dee396a6b51
Paris64 <ll8858261@gmail.com>
2025-10-08 10:48:23 +0300

    Добавление валидации ввода пользователя для требования минимальной длины текста

--- ISIP323_Kosheleva/Program.cs
+++ ISIP323_Kosheleva/Program.cs
@@ -1,63 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-public class TextStatistics
+Console.Write("Введите текст на русском(минимум 100 символов): ");
+string Text = Console.ReadLine();
+while (Text.Length < 100)
 {
-    public string Text { get; set; }
-    public int WordCount { get; set; }
-    public string ShortestWord { get; set; }
-    public int SentenceCount { get; set; }
-    public int VowelCount { get; set; }
-    public int ConsonantCount { get; set; }
-    public string LongestWord { get; set; }
-    public Dictionary<char, int> LetterFrequency { get; set; }
-    public DateTime AnalysisDate { get; set; }
+    Console.WriteLine("Слишком короткий текст!");
+    Console.Write("Введите текст на русском(минимум 100 символов): ");
+    Text = Console.ReadLine();
 
-    public TextStatistics()
-    {
-        LetterFrequency = new Dictionary<char, int>();
-        ShortestWord = "";
-        LongestWord = "";
-    }
-
-    public void DisplayStatistics()
-    {
-        Console.WriteLine("\n=== СТАТИСТИКА ТЕКСТА ===");
-        Console.WriteLine($"Дата анализа: {AnalysisDate}");
-        Console.WriteLine($"Общее количество слов: {WordCount}");
-        Console.WriteLine($"Количество предложений: {SentenceCount}");
-        Console.WriteLine($"Самое короткое слово: '{ShortestWord}'");
-        Console.WriteLine($"Самое длинное слово: '{LongestWord}'");
-        Console.WriteLine($"Гласные буквы: {VowelCount}");
-        Console.WriteLine($"Согласные буквы: {ConsonantCount}");
-
-        Console.WriteLine("\nЧастота букв:");
-
-        List<KeyValuePair<char, int>> sortedEntries = new List<KeyValuePair<char, int>>();
-        foreach (var entry in LetterFrequency)
-        {
-            sortedEntries.Add(entry);
-        }
-
-        // Пузырьковая сортировка по убыванию частоты
-        for (int i = 0; i < sortedEntries.Count - 1; i++)
-        {
-            for (int j = 0; j < sortedEntries.Count - i - 1; j++)
-            {
-                if (sortedEntries[j].Value < sortedEntries[j + 1].Value)
-                {
-                    var temp = sortedEntries[j];
-                    sortedEntries[j] = sortedEntries[j + 1];
-                    sortedEntries[j + 1] = temp;
-                }
-            }
-        }
-
-        foreach (var entry in sortedEntries)
-        {
-            Console.WriteLine($"'{entry.Key}': {entry.Value}");
-        }
-        Console.WriteLine("========================\n");
-    }
 }
