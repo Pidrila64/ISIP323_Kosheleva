d717f6caab294a962f77e3951ccfa6aea1537771
Paris64 <ll8858261@gmail.com>
2025-10-21 22:48:29 +0300

    декомпозиция бебе

--- ISIP323_Kosheleva/Program.cs
+++ ISIP323_Kosheleva/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+class Weapon
+{
+    
+}
+class Armor
+{
+    
+}
+class Player
+{
+    
+}
+
+class Enemy
+{
+    
+}
+class Goblin : Enemy
+{
+    
+}
+
+class Skelet : Enemy
+{
+    
+}
+
+class Mag : Enemy
+{
+    
+}
+
+class GoblinBoss : Goblin
+{
+    
+}
+
+class SkeletBossKova : Skelet
+{
+    
+}
+
+class SkeletBossPest : Skelet
+{
+    
+}
+
+class MagBoss : Mag
+{
+    
+}
+
+class Game
+{
+
+} 
+
+class Program
+{
+    
+}
