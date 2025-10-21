425c6b7181e06a8d45110a6577d9bb9a10c55912
Paris64 <ll8858261@gmail.com>
2025-10-21 22:52:01 +0300

    написан класс брони, оружия, игрока и его методы

--- ISIP323_Kosheleva/Program.cs
+++ ISIP323_Kosheleva/Program.cs
@@ -2,15 +2,73 @@
 
 class Weapon
 {
-    
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public Weapon(string name, int damage)
+    {
+        Name = name;
+        Damage = damage;
+    }
+
 }
 class Armor
 {
-    
+    public string Name { get; set; }
+    public int Defense { get; set; }
+    public Armor(string name, int defense)
+    {
+        Name = name;
+        Defense = defense;
+    }
+
 }
 class Player
 {
-    
+    public string Name;
+    public int MaxHP;
+    public int HP;
+    public bool IsFrozen = false;
+    public bool BlockNextAttack = false;
+    public Weapon Weapon { get; set; }
+    public Armor Armor { get; set; }
+    public Player(string name, int hp, Weapon weapon, Armor armor)
+    {
+        Name = name;
+        MaxHP = hp;
+        HP = hp;
+        Weapon = weapon;
+        Armor = armor;
+    }
+
+    public void Heal()
+    {
+       
+    }
+
+    public void EquipWeapon(Weapon newWeapon)
+    {
+        
+    }
+
+    public void EquipArmor(Armor newArmor)
+    {
+        
+    }
+
+    public bool TryDodge(Random rnd)
+    {
+        
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        
+    }
+
+    public bool IsAlive()
+    {
+    }
+
 }
 
 class Enemy
