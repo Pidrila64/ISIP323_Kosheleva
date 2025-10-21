d722c210d4ce77a20e4a852280232800b678e85d
Paris64 <ll8858261@gmail.com>
2025-10-21 22:57:05 +0300

    заполнен основной класс враг (enemy) и его подклассы

--- ISIP323_Kosheleva/Program.cs
+++ ISIP323_Kosheleva/Program.cs
@@ -107,41 +107,145 @@ class Player
 
 class Enemy
 {
-    
+    public string Name;
+    public int MaxHP;
+    public int CurrentHP;
+    public int Attack;
+    public int Defense;
+    public bool IgnoreDefense = false;
+    public bool HasCrit = false;
+    public int CritChance = 0;
+    public bool CanFreeze = false;
+    public int FreezeChance = 0;
+
+    public Enemy(int hp, int attack, int defense)
+    {
+        MaxHP = hp;
+        CurrentHP = hp;
+        Attack = attack;
+        Defense = defense;
+    }
+
+    public virtual void AttackPlayer(Player player, Random rnd)
+    {
+        int dmg = Attack;
+        if (!IgnoreDefense)
+            dmg -= player.Armor.Defense;
+
+        if (player.BlockNextAttack)
+        {
+            int blockPercent = rnd.Next(70, 101);
+            dmg = dmg * (100 - blockPercent) / 100;
+            player.BlockNextAttack = false;
+            Console.WriteLine($"Блок уменьшил получаемый урон на {blockPercent}%!");
+        }
+
+        if (HasCrit && rnd.Next(100) < CritChance)
+        {
+            dmg *= 2;
+            Console.WriteLine($"{Name} нанес критический удар!");
+        }
+
+        if (dmg < 1) dmg = 1;
+        player.TakeDamage(dmg);
+
+        if (CanFreeze && rnd.Next(100) < FreezeChance)
+        {
+            player.IsFrozen = true;
+            Console.WriteLine($"{Name} наложил заморозку! Вы пропустите следующий ход.");
+        }
+    }
+
+    public bool IsAlive()
+    {
+        return CurrentHP > 0;
+    }
+
 }
 class Goblin : Enemy
 {
-    
+    public Goblin() : base(30, 5, 2)
+    {
+        Name = "Гоблин";
+        HasCrit = true;
+        CritChance = 20;
+    }
+
 }
 
 class Skelet : Enemy
 {
-    
+    public Skelet() : base(40, 6, 3)
+    {
+        Name = "Скелет";
+        IgnoreDefense = true;
+    }
+
 }
 
 class Mag : Enemy
 {
-    
+    public Mag() : base(25, 4, 2)
+    {
+        Name = "Маг";
+        CanFreeze = true;
+        FreezeChance = 20;
+    }
+
 }
 
 class GoblinBoss : Goblin
 {
-    
+    public GoblinBoss()
+    {
+        Name = "ВВГ Гоблин-Босс";
+        MaxHP = 30 * 2;
+        CurrentHP = MaxHP;
+        Attack = (int)(5 * 1.5);
+        Defense = (int)(2 * 1.2);
+        CritChance += 10;
+    }
+
 }
 
 class SkeletBossKova : Skelet
 {
-    
+    public SkeletBossKova()
+    {
+        Name = "Скелет-Ковальский";
+        MaxHP = (int)(40 * 2.5);
+        CurrentHP = MaxHP;
+        Attack = (int)(6 * 1.3);
+        Defense = (int)(3 * 1.4);
+    }
+
 }
 
 class SkeletBossPest : Skelet
 {
-    
+    public SkeletBossPest()
+    {
+        Name = "Скелет-Пестов";
+        MaxHP = (int)(40 * 1.3);
+        CurrentHP = MaxHP;
+        Attack = (int)(6 * 1.8);
+        Defense = (int)(3 * 0.6);
+    }
+
 }
 
 class MagBoss : Mag
 {
-    
+    public MagBoss()
+    {
+        Name = "Архимаг C++";
+        MaxHP = (int)(25 * 1.8);
+        CurrentHP = MaxHP;
+        Attack = (int)(4 * 1.6);
+        Defense = (int)(2 * 1.1);
+        FreezeChance += 10;
+    }
+
 }
 
 class Game
